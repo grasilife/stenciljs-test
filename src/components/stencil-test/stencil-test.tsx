068f333919d149bdc0cd1499f6405eaca8ecc035
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-10-31 00:17:21 +0800

    完善组件

--- src/components/stencil-test/stencil-test.tsx
+++ src/components/stencil-test/stencil-test.tsx
@@ -2,11 +2,11 @@ import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
-  tag: 'my-component',
-  styleUrl: 'my-component.css',
+  tag: 'stencil-test',
+  styleUrl: 'stencil-test.css',
   shadow: true,
 })
-export class MyComponent {
+export class stencilTest {
   /**
    * The first name
    */
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,15 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
+  //写不好会404,真不知道为啥
   namespace: 'test',
-  taskQueue: 'async',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle'
+      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
@@ -18,5 +18,8 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+  ],
+  bundles: [
+    { components: ['stencil-test'] }
   ],
 };
